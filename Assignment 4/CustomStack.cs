9a944243461dd2608ad5c27eb4d7b26472535e62
ravitejainjedu <41326065+ravitejainjedu@users.noreply.github.com>
2025-08-08 21:36:56 -0500

    Create Assignment 4

---
+++ Assignment 4/CustomStack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4
+{
+    public class CustomStack<T>
+    {
+        private T[] _items = new T[4];     // initial capacity
+        private int _count;                // current number of elements
+
+        // returns number of items in the stack
+        public int Count()
+        {
+            return _count;
+        }
+
+        // pushes an item onto the top of the stack
+        public void Push(T item)
+        {
+            if (_count == _items.Length) // resize when full
+            {
+                Array.Resize(ref _items, _items.Length * 2);
+            }
+            _items[_count++] = item;
+        }
+
+        // pops and returns the top item, throws if empty
+        public T Pop()
+        {
+            if (_count == 0)
+                throw new InvalidOperationException("Stack is empty");
+
+            T top = _items[--_count];
+            _items[_count] = default;     // clear reference
+            return top;
+
+        }
+    }
+}
---
+++ Assignment 4/GenericRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4
+{
+    // generic repo using an in-memory list
+    public class GenericRepository<T> : IRepository<T> where T : class, IEntity, new()
+    {
+        // backing store – replace with DbContext or any provider
+        private readonly List<T> _items = new List<T>();
+
+        // add new entity
+        public void Add(T item)
+        {
+            _items.Add(item);
+        }
+
+        // remove existing entity
+        public void Remove(T item)
+        {
+            _items.Remove(item);
+        }
+
+        // no-op for in-memory, hook into persistence here
+        public void Save()
+        {
+            // e.g. _dbContext.SaveChanges();
+        }
+
+        // return all entities
+        public IEnumerable<T> GetAll()
+        {
+            return _items.AsReadOnly();
+        }
+
+        // find by Id property
+        public T GetById(int id)
+        {
+            return _items.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
